c3249ffc20b8198833203d367f7216e80f4e5778
sikadi-darling <#>
2025-06-30 02:26:37 +0800

    test 2

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // 允许外部访问
+    port: 5173,
+  },
   plugins: [
     vue(),
     vueDevTools(),
